e65f89cb5f580b4cbb2968812b19ac894cdf6ddc
Aliaksiej Razumaŭ <belarusaed@gmail.com>
2025-12-07 14:30:21 +0100

    Vercel again

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 // @ts-check
 import react from '@astrojs/react';
-import vercel from '@astrojs/vercel/serverless';
+import vercel from '@astrojs/vercel';
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'astro/config';
 
@@ -13,5 +13,5 @@ export default defineConfig({
   integrations: [react()],
 
   output: 'server',
-  adapter: vercel({}),
+  adapter: vercel(),
 });
