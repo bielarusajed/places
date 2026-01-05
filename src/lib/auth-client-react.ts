eeeb785e5f0ca71ac820a9f6c394a0311406f11a
Aliaksiej Razumaŭ <belarusaed@gmail.com>
2026-01-05 16:30:21 +0100

    ???

--- src/lib/auth-client-react.ts
+++ src/lib/auth-client-react.ts
@@ -1,5 +1,3 @@
 import { createAuthClient } from 'better-auth/react';
 
-export const authClient = createAuthClient({
-  baseURL: import.meta.env.SITE ?? 'http://localhost:4321',
-});
+export const authClient = createAuthClient({});
